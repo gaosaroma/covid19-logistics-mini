c656bc7042a3506e04bfffcb7ebecc08d6a9b725
gaosaroma <gaosaroma@gmail.com>
2020-12-30 18:50:57 +0800

    init tab-bar
    init tab-bar for different custom

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,77 @@
+// custom-tab-bar/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selected: 0,
+    color: "#b5b5b5",
+    selectedColor: "#fa8582",
+    list:[]
+  },
+  lifetimes: {
+  // 在组件实例进入页面节点树时赋值
+    attached () {
+      this.setData({
+        list: app.globalData.list
+      })
+    },
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
