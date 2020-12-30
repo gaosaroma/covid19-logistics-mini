182e9ff59004cc08f8216e5d1b9302d2a08e3b34
Mustafa <920998500@qq.com>
2020-12-30 19:03:50 +0800

    api_merge

--- pages/applyRisk/applyRisk.js
+++ pages/applyRisk/applyRisk.js
@@ -5,9 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    openid: '',
-    logistics_id: '',
+    user_info: {},
+    logistics_id: 1,
     risk_description: '',
+    station_name: "仓库：上海浦东新区中转站",
     transportationSArray: [
       ["仓库", "交通工具"],
       ["江苏省", "上海市"],
@@ -15,7 +16,7 @@ Page({
     ],
     transportationIndex: [0, 0, 0],
     index: null,
-    picker: ['中风险', '中高风险', '高风险'],
+    picker: ['中风险', '高风险'],
     imgList: [],
   },
 
@@ -33,75 +34,17 @@ Page({
   },
 
   LogisticsIdInput(e) {
+    console.log(e);
     this.setData({
       logistics_id: e.detail.value
     })
   },
 
   RiskDesInput(e) {
+    console.log(e);
     this.setData({
       risk_description: e.detail.value
     })
-  },
-
-  MultiChange(e) {
-    this.setData({
-      transportationIndex: e.detail.value
-    })
-  },
-
-  MultiColumnChange(e) {
-    let data = {
-      transportationSArray: this.data.transportationSArray,
-      transportationIndex: this.data.transportationIndex
-    };
-    data.transportationIndex[e.detail.column] = e.detail.value;
-    switch (e.detail.column) {
-      case 0:
-        switch (data.transportationIndex[0]) {
-          case 0:
-            data.transportationSArray[1] = ["江苏省", "上海市"];
-            data.transportationSArray[2] = ["太仓市中转站", '苏州市中转站'];
-            break;
-          case 1:
-            data.transportationSArray[1] = ['火车', '飞机', '汽车'];
-            data.transportationSArray[2] = ['Z1234', 'D900', 'K666'];
-            break;
-        }
-        data.transportationIndex[1] = 0;
-        data.transportationIndex[2] = 0;
-        break;
-      case 1:
-        switch (data.transportationIndex[0]) {
-          case 0:
-            switch (data.transportationIndex[1]) {
-              case 0:
-                data.transportationSArray[2] = ["太仓市中转站", '苏州市中转站'];
-                break;
-              case 1:
-                data.transportationSArray[2] = ['嘉定区中转站', '浦东新区中转站'];
-                break;
-            }
-            break;
-          case 1:
-            switch (data.transportationIndex[1]) {
-              case 0:
-                data.transportationSArray[2] = ['Z1234', 'D900', 'K666'];
-                break;
-              case 1:
-                data.transportationSArray[2] = ['HO1163', 'JI2917'];
-                break;
-              case 2:
-                data.transportationSArray[2] = ['沪A666', '苏B8237', '苏C5236'];
-                break;
-            }
-            break;
-        }
-        data.transportationIndex[2] = 0;
-        break;
-    
-    }
-    this.setData(data);
   },
 
   PickerChange(e) {
@@ -136,6 +79,7 @@ Page({
       current: e.currentTarget.dataset.url
     });
   },
+
   DelImg(e) {
     wx.showModal({
       title: '',
@@ -156,19 +100,25 @@ Page({
   submit(e) {
     console.log(e);
     var that = this;
-    var array = that.data.transportationSArray;
-    var indices = that.data.transportationIndex;
-    var cur_add = array[0][indices[0]]+ array[1][indices[1]]+array[2][indices[2]];
-    console.log(that.data)
+    var app = getApp();
+    var data = that.data;
+    var globalData = app.globalData;
+    var risk_type = 0;
+    if (data.picker[data.index]=='中风险') {risk_type=1}
+    else if (data.picker[data.index]=='高风险') {risk_type=2}
     wx.request({
-      url: 'https://host/apply/risk',
+      url: globalData.base_url+'apply/submitRisk',
       data: {
-        user_id: that.openid,
-        logistics_id: that.logistics_id,
-        cur_state: that.picker[that.index],
-        cur_addr: cur_add,
-        risk_description: that.risk_description,
-        attached_images: that.imgList
+        // submitter: data.user_info.work_id,
+        'submitter': 7,
+        'logisticsId': data.logistics_id,
+        // cur_state: data.picker[data.index],
+        // cur_addr: cur_add,
+        'stationId': 1,
+        'submitComment': data.risk_description,
+        'attached_images': data.imgList,
+        'type': risk_type,
+        'applyType': 1
       },
       header: {
         'content-type': 'application/json'
@@ -176,7 +126,24 @@ Page({
       method: 'POST',
       success: function(result) {
         console.log(result);
-      }
+        var code = result.data.code;
+        var index = result.data.resultObjects[0].id;
+        if (code==200){
+          wx.showToast({
+            title: '申报成功',
+            icon: 'succes',
+            duration: 1000,
+            mask:true,
+            success:()=>{
+              setTimeout(()=> {
+                  wx.navigateTo({
+                      url:'/pages/viewMyApply/viewMyApply?id='+index
+                  })
+              },1000)
+          }
+          })
+        }
+      },
     })
   },
 
@@ -186,62 +153,13 @@ Page({
   onLoad: function (options) {
     var that = this;
     wx.getStorageSync({
-      key: 'openid',
+      key: 'userinfo',
       success (res) {
         console.log(res.data);
         that.setData({
-          openid: res.data
+          user_info: res.data
         })
       }
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
